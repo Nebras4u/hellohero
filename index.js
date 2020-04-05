471e19dfad83cd0102f1c4e696d86d55b5b763b6
nebras <nebras4u@gmail.com>
2020-04-05 06:46:08 -0700

    dd

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 var express = require('express');
 var app = express();
 
-require('dotenv').config()
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.set('port', (process.env.PORT || 5000));
-app.engine('html', require('ejs').renderFile);
+// set the view engine to ejs
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/public');
 
-app.get('/', function(request, response) {
-  var env = process.env.APP_ENV;
-  if (env == 'staging') {
-    var envName = 'staging'
-  } else if (env == 'production') {
-    var envName = 'production'
-  } else {
-    var envName = 'review app'
-  }
-  response.render('index.html', { env: envName});
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app running at localhost:" + app.get('port'));
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
